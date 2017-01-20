e5a6b8f1039d3094dd22c3c61951e0cf2d53ea49
Mike Burrows <m3b@google.com>
2017-01-19 16:38:16 -0800

    Add copyright headers on source files.

--- tools/mkmakefile.sh
+++ tools/mkmakefile.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Attempt to create a subdirectory in builds/ suitable for
 # building nsync on this machine.   Works on many Unix-like systems,
 # but for some you should create the Makefile by hand.
